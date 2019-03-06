8b8e59d71902424995db8542d4a1c75c0df8ab82
Serven Maraghi <maraghiserven@gmail.com>
2019-03-05 21:06:09 -0500

    update info feature added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", init)
 
 function init() {
     getPainter()
+    getPaintings()
 }
 
 function getPainter(){
@@ -12,23 +13,88 @@ function getPainter(){
 
 function renderPainter(painter){
     painter = painter[0]
-    
+
     let bioDiv = document.querySelector('#bio')
 
     let h2 = document.createElement('h2')
     let pBio = document.createElement('p')
     let pContact = document.createElement('p')
-    let editBtn = document.createElement('button')
 
     h2.classList.add('painter-name')
     pBio.classList.add('painter-bio')
     pContact.classList.add('painter-contact')
-    editBtn.classList.add('edit-btn')
+ 
 
     h2.innerText = painter.name
     pBio.innerText = painter.bio
     pContact.innerText = painter.contact
-    editBtn.innerText = "Update Info"
+    
 
-    bioDiv.append(h2, pBio, pContact, editBtn)
+    bioDiv.append(h2, pBio, pContact)
+
+    //following is to populate values into update form
+
+    document.getElementsByName('contact')[0].value = painter.contact
+    document.getElementsByName('bio')[0].value = painter.bio
+    document.getElementById('form-id').value = painter.id
+
 }
+
+function editPainterBio(){
+    let id = $("#form-id").val()
+    
+    let data = {
+        bio: $("#form-bio").val(), 
+        contact: $("#form-contact").val()
+    }
+
+    fetch(`http://localhost:3000/painter/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+    .then(json => updatePainter(json))
+
+}
+
+function updatePainter(painter) {
+    
+    let painterBio = document.getElementsByClassName('painter-bio')[0]
+    let painterContact = document.getElementsByClassName('painter-contact')[0]
+ 
+    painterBio.value = painter.bio
+    painterContact.value = painter.contact
+}
+
+function getPaintings() {
+    
+    fetch("http://localhost:3000/paintings")
+    .then(res => res.json())
+    .then(painting => painting.forEach(renderPaintings))
+}
+
+function renderPaintings(painting) {
+    
+    let rightPanel = document.getElementsByClassName("right-panel")[0]
+    let paintingDiv = document.createElement('div')
+    let h4 = document.createElement('h4')
+    let img = document.createElement('img')
+
+    h4.classList.add("painting-title")
+    img.classList.add("painting-img")
+    paintingDiv.classList.add("painting")
+
+    h4.innerText = painting.title
+    img.src = painting.image
+
+
+    paintingDiv.append(img, h4)
+    rightPanel.appendChild(paintingDiv)
+
+
+    // paintingDiv.addEventListener('click', deletePainting)
+}
+
